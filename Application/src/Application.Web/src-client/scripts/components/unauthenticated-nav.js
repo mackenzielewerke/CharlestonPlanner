7abe9d30907ce65d62ea538e1cd835fb4f24d206
Cameron Bolus <cameronbolus@Camerons-MacBook-Pro.local>
2017-03-15 17:22:27 -0400

    cameron made home button

--- Application/src/Application.Web/src-client/scripts/components/unauthenticated-nav.js
+++ Application/src/Application.Web/src-client/scripts/components/unauthenticated-nav.js
@@ -12,11 +12,16 @@ export const RegularNavComponent = React.createClass({
 		window.location.hash="account/register"
 	},
 
+	_handleHome: function(evt){
+		window.location.hash=""
+	},
+
 	render: function(){
 		return (
 			<nav className="reg-navigation">
         <img className="logo" src="./images/CharlestonPlanner.png" alt=""></img>
         <div className="sign-in">
+					<div className="hover-color spaced-out" onClick={this._handleHome}>Home</div>
           <div className="hover-color spaced-out" onClick={this._handleSignUp}>Sign Up</div>
           <div className="hover-color spaced-out" onClick={this._handleLogIn}>Log In</div>
         </div>
