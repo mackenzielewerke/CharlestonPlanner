431c45b73dbd20ca2c7201235779ac8d02b9ae02
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-03-15 14:54:12 -0400

    click works for sign in and register

--- Application/src/Application.Web/src-client/scripts/components/unauthenticated-nav.js
+++ Application/src/Application.Web/src-client/scripts/components/unauthenticated-nav.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 export const RegularNavComponent = React.createClass({
+
+	_handleLogIn: function(evt){
+	window.location.hash = "account/login"
+},
+
+	_handleSignUp: function(evt){
+		window.location.hash="account/register"
+	},
+
 	render: function(){
 		return (
 			<nav className="reg-navigation">
         <img className="logo" src="./images/CharlestonPlanner.png" alt=""></img>
         <div className="sign-in">
-          <div className="hover-color spaced-out">Sign Up</div>
-          <div className="hover-color spaced-out">Log In</div>
+          <div className="hover-color spaced-out" onClick={this._handleSignUp}>Sign Up</div>
+          <div className="hover-color spaced-out" onClick={this._handleLogIn}>Log In</div>
         </div>
 			</nav>
 		)
