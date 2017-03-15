f45738f3870ca1b34414d135298f9f0bb2660f74
Cameron Bolus <cameronbolus@Camerons-MacBook-Pro.local>
2017-03-15 11:47:49 -0400

    new props

--- Application/src/Application.Web/src-client/scripts/components/home-carousel.js
+++ Application/src/Application.Web/src-client/scripts/components/home-carousel.js
@@ -1,26 +1,26 @@
-// import ReactDOM from 'react-dom'
-// import React from 'react'
-//
-// var Slider = require('react-slick');
-//
-// var SimpleSlider = React.createClass({
-//   render: function () {
-//     var settings = {
-//       dots: true,
-//       infinite: true,
-//       speed: 500,
-//       slidesToShow: 1,
-//       slidesToScroll: 1
-//     };
-//     return (
-//       <Slider {...settings}>
-//         <div><h3>1</h3></div>
-//         <div><h3>2</h3></div>
-//         <div><h3>3</h3></div>
-//         <div><h3>4</h3></div>
-//         <div><h3>5</h3></div>
-//         <div><h3>6</h3></div>
-//       </Slider>
-//     );
-//   }
-// });
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+var Slider = require('react-slick');
+
+var SimpleSlider = React.createClass({
+  render: function () {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <Slider {...settings}>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+        <div><h3>6</h3></div>
+      </Slider>
+    );
+  }
+});
