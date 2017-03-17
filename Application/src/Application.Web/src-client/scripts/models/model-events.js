108603aa628071008e4ed953ba4c833a85759a49
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-03-17 16:02:09 -0400

    still playing with models shannon

--- Application/src/Application.Web/src-client/scripts/models/model-events.js
+++ Application/src/Application.Web/src-client/scripts/models/model-events.js
@@ -1,12 +1,21 @@
 import Backbone from 'backbone'
 
 
-export const EventsModel = Backbone.Model.extend({
-	urlRoot: 'https://api.myjson.com/bins/drz1f',
+export const PretendEventsModel = Backbone.Model.extend({
+	url: 'https://api.myjson.com/bins/drz1f',
 	// idAttribute: '_id'
 })
 
-export const EventsCollection = Backbone.Collection.extend({
-	model: EventsModel,
-	url: 'https://api.myjson.com/bins/drz1f'
+export const PretendEventsCollection = Backbone.Collection.extend({
+	model: PretendEventsModel
+})
+
+
+export const EventsModel= Backbone.Model.extend({
+	urlRoot: 'api/events'
+	idAttribute: '_id'
+})
+
+export const EventsCollection= Backbone.Collection.extend({
+	model: EventsModel
 })
