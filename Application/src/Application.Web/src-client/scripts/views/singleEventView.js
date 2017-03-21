b34f87782c03421af91149f643e4bd54f46f4e6e
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-03-21 19:43:19 -0400

    shannon changes to single view

--- Application/src/Application.Web/src-client/scripts/views/singleEventView.js
+++ Application/src/Application.Web/src-client/scripts/views/singleEventView.js
@@ -23,9 +23,19 @@ export const SingleEventView = React.createClass({
 	},
 
 	render: function(){
+		let component= this
+		console.log(this.props, "single view props??????????")
 		console.log('data??', this.props.oneEventsList)
 		console.log(this.state.eventsList.length)
+		let singleEventInfo = this.props.eventsList.filter(function(obj, i){
+			if(parseInt(component.props.singleEvent) === i){
+				return true
+			}
+		})[0]
+		// [{eventInfo: a;ldskfja;sdlf}]
+		// let singleEventInfo = {}
 		if (this.state.eventsList.length < 1){
+			console.log(this.props.oneEventsList)
 		console.log("executing")
 		return <p> loading </p>
 		}
