504bdbff4adbde7dddd9d6033868d84862d92a8d
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-03-22 15:35:55 -0400

    search progress

--- Application/src/Application.Web/src-client/scripts/views/homeView.js
+++ Application/src/Application.Web/src-client/scripts/views/homeView.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import {RegularNavComponent} from '../components/unauthenticated-nav.js'
 import {SearchBarComponent} from '../components/searchbar.js'
+import {STORE} from '../store.js'
+import {ACTIONS} from '../actions.js'
 // import {SimpleSlider} from '../components/home-carousel.js'
 
 export const HomePageView = React.createClass({
+
+	getInitialState: function(){
+		return STORE.getStoreData()
+	},
+
+	componentDidMount: function(){
+		let component = this;
+		console.log('ACTIONS.fetch()...')
+		ACTIONS.fetchAllEvents()
+
+	},
+
 	render: function(){
 		return (
       <div>
-           <SearchBarComponent/>
+           <SearchBarComponent eventsList={this.state.eventsList}/>
            <img src="./images/people.jpg" alt=""></img>
 				 	{/* <SimpleSlider/> */}
       </div>
